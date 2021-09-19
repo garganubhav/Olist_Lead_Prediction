a0e03c24c52bf596589a8bc28a22e259ccf22eaf
Anubhav Garg <anubhavgarg5@gmail.com>
2021-09-19 22:53:32 +0530

    Update register.sh

--- register.sh
+++ register.sh
@@ -1,4 +1,3 @@
 docker build . -t $BUILD_IMAGE_LOCAL
-export AWS_ECR="075156609000.dkr.ecr.us-west-2.amazonaws.com/dsi-mlops"
-docker tag $BUILD_IMAGE_LOCAL $AWS_ECR:$CI_COMMIT_SHA
-docker push $AWS_ECR:$CI_COMMIT_SHA
+docker tag $BUILD_IMAGE_LOCAL $CI_COMMIT_SHA
+docker push anubhavgarg/$CI_COMMIT_SHA
