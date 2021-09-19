4fd28a2e3c64a591ac6f056eccdd243c0cf117d8
Anubhav Garg <anubhavgarg5@gmail.com>
2021-09-19 22:55:31 +0530

    Update register.sh

--- register.sh
+++ register.sh
@@ -1,3 +1,3 @@
 docker build . -t $BUILD_IMAGE_LOCAL
-docker tag $BUILD_IMAGE_LOCAL $CI_COMMIT_SHA
-docker push anubhavgarg/$CI_COMMIT_SHA
+docker tag $BUILD_IMAGE_LOCAL $GITHUB_SHA
+docker push anubhavgarg/$GITHUB_SHA
